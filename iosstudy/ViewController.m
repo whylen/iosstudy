2a12b823b7b53ee05b9857473985c5704b29baa5
whylen <whylen@aliyun.com>
2018-01-02 23:34:41 +0800

    UIVIewController生命周期展示

--- iosstudy/ViewController.m
+++ iosstudy/ViewController.m
@@ -7,23 +7,83 @@
 //
 
 #import "ViewController.h"
-
+int tip=0;
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
++(void)initialize {
+    [super initialize];
+    NSLog(@"%d initialize", ++tip);
+}
+
+-(instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    NSLog(@"%d init", ++tip);
+    return self;
+}
+
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        
+    }
+    NSLog(@"%d initWithCoder", ++tip);
+    return self;
+}
+
+-(void)awakeFromNib {
+    [super awakeFromNib];
+    NSLog(@"%d awakeFromNib", ++tip);
+}
+
+-(void)loadView {
+    [super loadView];
+    NSLog(@"%d loadView", ++tip);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"%d viewDidLoad", ++tip);
 }
 
+-(void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    NSLog(@"%d viewDidLayoutSubviews", ++tip);
+}
 
-- (void)didReceiveMemoryWarning {
+-(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%d didReceiveMemoryWarning", ++tip);
 }
 
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    NSLog(@"%d viewWillAppear", ++tip);
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    NSLog(@"%d viewDidAppear", ++tip);
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    NSLog(@"%d viewWillDisappear", ++tip);
+}
+
+-(void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"%d viewDidDisappear", ++tip);
+}
+
+-(void)dealloc {
+    NSLog(@"%d dealloc", ++tip);
+}
 
 @end
