312c5c20306bd86bd96b64dab25a4ad1726fb2d9
whylen <whylen@aliyun.com>
2018-01-02 22:47:32 +0800

    Initial Commit

---
+++ iosstudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosstudy
+//
+//  Created by lauwhylen on 2018/1/2.
+//  Copyright © 2018年 com.ZNTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosstudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosstudy
+//
+//  Created by lauwhylen on 2018/1/2.
+//  Copyright © 2018年 com.ZNTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
